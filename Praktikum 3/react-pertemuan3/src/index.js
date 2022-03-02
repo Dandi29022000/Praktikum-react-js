938f22c6373d26980219246507242f1b1d4c0f5b
Dandi29022000 <=>
2022-03-02 10:03:04 +0700

    Praktikum 3

---
+++ Praktikum 3/react-pertemuan3/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Test from './Test';
+import HelloComponent from './component/HelloComponent';
+import Login from './Login';
+
+// function HelloComponent(){
+//   return <p>HelloComponent</p>
+// }
+
+// class StateFullComponent extends React.Component {
+//   render() {
+//     return <p>Statefull Component</p>;
+//   }
+// }
+
+// ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+ReactDOM.render(<Login />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
