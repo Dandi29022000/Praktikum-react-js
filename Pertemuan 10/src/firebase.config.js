e68aa017df9af4e1ccee243849109af8f28fb0fa
dandiagungs29 <60720839+Dandi29022000@users.noreply.github.com>
2022-05-18 11:40:11 +0700

    Praktikum Modul 10

---
+++ Pertemuan 10/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDu86Kprje_TJejjfBmDL7-Npf0UShmKHs",
+    authDomain: "week10-react-firebase.firebaseapp.com",
+    projectId: "week10-react-firebase",
+    storageBucket: "week10-react-firebase.appspot.com",
+    messagingSenderId: "264093320199",
+    appId: "1:264093320199:web:741b518e4a862e259f16a2",
+    measurementId: "G-KMZZMN314X"
+};
