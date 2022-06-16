39134e112582ac6b734bdc4fd93a6ed2eb10ee67
dandiagungs29 <60720839+Dandi29022000@users.noreply.github.com>
2022-06-16 11:05:20 +0700

    Frontend

---
+++ UAS/E-Commerce/E-Commerce Frontend/src/contents/About.js
@@ -0,0 +1,40 @@
+import {Layout, Breadcrumb, Image, List } from 'antd'
+import React from 'react'
+import profileImg from '../Assets/polinema.jpg'
+
+const {Content} = Layout
+
+const About = () => {
+    return(
+        <Content style={{ margin: '0 16px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Shop</Breadcrumb.Item>
+            <Breadcrumb.Item>About</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 400, minWidth:300}}>
+            <h2 style={{textAlign:'center'}}>About Me</h2>
+            <div style={{padding:50,width:'auto', display:'flex', border:'1px solid lightgray', borderRadius:10}}>
+                <Image src={profileImg} style={{maxHeight: 200, maxWidth:200, height:'auto', width:'auto'}} />
+                <div style={{flexWrap: 'wrap', marginLeft: 10, padding:20}}>
+                    <p style={{fontSize: 15}}>
+                        Hai, perkenalkan kelompok kami :<br/>
+                        <p>Hendri Daffa Athaya</p>
+                        <p>Moch. Farchan Agil</p>
+                        <p>M. Riefky Ariyanto</p>
+                        Mahasiswa aktif di Politeknik Negeri Malang<br/>
+                        Jurusan Teknologi Informasi<br/>
+                        jika ada pertanyaan silahkan kontak kami dibawah ini :
+                    </p>
+                    <List >
+                            <List.Item key='github' style={{padding:'0px'}}>
+                                Github: <a href='https://github.com/Hendridaffa' target='#blank'>https://github.com/Hendridaffa</a>
+                            </List.Item>
+                        </List>
+                </div>
+            </div>
+        </div>
+        </Content>
+    )
+}
+
+export default About
---
+++ UAS/E-Commerce/E-Commerce Frontend/src/firebase/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDw4X0LOYwXnfePNpJ7kzS6--3voykRpFo",
+  authDomain: "uas-pbf-1c4e0.firebaseapp.com",
+  databaseURL: "https://uas-pbf-1c4e0-default-rtdb.firebaseio.com",
+  projectId: "uas-pbf-1c4e0",
+  storageBucket: "uas-pbf-1c4e0.appspot.com",
+  messagingSenderId: "419297249858",
+  appId: "1:419297249858:web:f341925d82b68440843bdb",
+  measurementId: "G-R4J1R5D5NL"
+};
+
+export default firebaseConfig
